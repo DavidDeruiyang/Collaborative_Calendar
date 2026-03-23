b8d5695fd757851e3e62c10e7651eabfc3d8ff31
tianpeif <tpf96@hotmail.com>
2026-03-22 22:49:42 -0400

    Change image type to latest. Update deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,11 @@
+echo [1/3] Building Docker image...
+docker build -t pcdelight/calendar-api:latest .
+
+echo [2/3] Pushing image to Docker Hub...
+docker push pcdelight/calendar-api:latest
+
+echo [3/3] Syncing Kubernetes manifests...
+
 kubectl apply -f .\calendar-db-secret.yaml
 kubectl apply -f .\calendar-api-secret.yaml
 kubectl apply -f .\calendar-mail-secret.yaml
@@ -13,3 +21,6 @@ kubectl apply -f .\calendar-redis-service.yaml
 
 kubectl apply -f .\calendar-api-deployment.yaml
 kubectl apply -f .\calendar-api-service.yaml
+
+echo [*] Triggering rolling restart for API...
+kubectl rollout restart deployment calendar-api-deployment
